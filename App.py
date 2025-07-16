d700886a1ce0e8dbe9bba0c130c273aed9b88c47
PaoPaoGuti <ap.guti0@gmail.com>
2025-07-16 16:09:25 +0200

    Modificación de filtro de periodicidad

--- App.py
+++ App.py
@@ -44,17 +44,19 @@ with st.sidebar:
     #}
 
     def clasificar_frecuencia(f):
+    f_lower = str(f).lower()
 
-        if "semanal" in f:
-            return "Semanal"
-        elif "y" in f:
-            return "Otros"
-        elif any(palabra in f for palabra in ["ultimo", "primer", "segundo", "tercer", "mensual"]):
-            return "Una vez al mes"
-        else:
-            return "Otros"
+    if "semanal" in f_lower:
+        return "Semanal"
+    elif "y" in f_lower:
+        return "Otros"
+    elif any(p in f_lower for p in ["ultimo", "primer", "segundo", "tercer", "mensual"]):
+        return "Una vez al mes"
+    else:
+        return "Otros"
 
-    df['Frecuencia_categoria'] = df['Frecuencia'].apply(clasificar_frecuencia)
+df['Frecuencia_categoria'] = df['Frecuencia'].apply(clasificar_frecuencia)
+
 
 df_filtrado = df.copy()
 
